69ea43882597c39da7eede8307b465afa56206ba
Matteo Gavoni <matteo.gavoni@gmail.com>
2024-05-27 16:50:11 +0200

    MG: corrected redirect of some actions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -297,7 +297,7 @@ class PostController extends Controller
                 });
             }
 
-            return back()->with('message', 'Notification created successfully!');
+            return $this->redirect('/forum/' . $category->id . '/' . $post->id)->with('message', 'Notification created successfully!');
         }
     }
 
@@ -367,7 +367,7 @@ class PostController extends Controller
 
             $post->update($formFields);
 
-            return $this->show($category, $post)->with('message', 'Post updated successfully!');
+            return $this->redirect('/forum/' . $category->id . '/' . $post->id)->with('message', 'Post updated successfully!');
         } else {
             abort(403, 'Unauthorized Action');
         }
