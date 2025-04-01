9329fa41c7728cb34868ee9992cf28e6e6bafafe
Matteo Gavoni <matteo.gavoni@426.agency>
2025-04-01 23:57:22 +0200

    MG: add path for listing

--- resources/views/mails/listing_reminder.blade.php
+++ resources/views/mails/listing_reminder.blade.php
@@ -3,7 +3,7 @@
 <p>You have a listing titled <strong>{{ $listing->title }}</strong> waiting to be viewed.</p>
 
 <p>
-    <a href="{{ route('listing.show', $listing) }}"
+    <a href="https://theboise.it/listings/{{ $listing->id }}"
        style="display: inline-block; padding: 10px 20px; background-color: #3490dc; color: white; text-decoration: none; border-radius: 5px;">
         View Listing
     </a>
