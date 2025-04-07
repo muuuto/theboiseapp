95644fc46bd0c34a1c13d2fe2388bdf383c51c46
Matteo Gavoni <matteo.gavoni@426.agency>
2025-04-08 00:41:37 +0200

    MG: add image to email

--- resources/views/mails/listing_reminder.blade.php
+++ resources/views/mails/listing_reminder.blade.php
@@ -4,6 +4,7 @@
 @foreach ($listings as $listing)
     <strong>{{ $listing->title }}</strong>
     
+    <img src="{{asset('storage/app/public/' . $listing->logo)}}" width="800" height="600" alt="">
     <p>
         <a href="https://theboise.it/listings/{{ $listing->id }}"
            style="display: inline-block; padding: 10px 20px; background-color: #3490dc; color: white; text-decoration: none; border-radius: 5px;">
