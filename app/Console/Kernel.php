b3e19edb9ca64939db295a1176e58521cc0a861e
matteogavoni <matteo.gavoni@alpitronic.it>
2025-04-09 15:31:58 +0200

    MG: add new anniversary job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('listings:send-reminders')->weeklyOn(0, '20:00');
-        // $schedule->command('notifications:anniversaries')->dailyAt('08:00');
+        $schedule->command('notifications:anniversaries')->dailyAt('09:00');
     }
 
     /**
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
